893509555c17bbb3c0e6fa3408e622a337d063f8
紅梅陽太 <powdersugar828828@gmail.com>
2020-05-07 19:09:59 +0900

    ビルド時の設定を追加。(たたき台)

--- app/next.config.js
+++ app/next.config.js
@@ -1,20 +1,36 @@
-// const withTypescript = require('@zeit/next-typescript');
-// const path = require('path');
-// const Dotenv = require('dotenv-webpack');
-// 
-// module.exports = withTypescript(
-//     webpack(config => {
-//         config.plugins = [
-//             new Dotenv({
-//                 path: path.join(__dirname, '.env'),
-//                 systemvars: true
-//             })
-//         ]
-//     })
-// );
+const axios = require('axios');
 require('dotenv').config();
 
 module.exports = {
+    exportPathMap: async function() {
+        const paths = {
+            '/': {page: '/'},
+        };
+
+        // const headers = {
+        //     headers: {'X-API-KEY': process.env.API_KEY}
+        // };
+
+        // const blogRes = await axios.get(
+        //     `https://karukichi-tech-blog.microcms.io/api/v1/blogs`,
+        //     headers,
+        // );
+        // const tagRes = await axios.get(
+        //     `https://karukichi-tech-blog.microcms.io/api/v1/tags`,
+        //     headers,
+        // );
+        // const blogs = blogRes.data.contents;
+        // blogs.forEach(blog => {
+        //     paths[`/blogs/${blog.id}`] = {page: '/blogs/[id]', query: {id: blog.id}};
+        // });
+
+        // const tags = tagRes.data.contents;
+        // tags.forEach(tag => {
+        //     paths[`/tags/${tag.id}`] = {page: '/tags/[id]', query: {id: tag.id}};
+        // })
+
+        return paths;
+    },
     env: {
         access_key: process.env.API_KEY,
     },
