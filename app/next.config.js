2d758f4e05d7b763a670a789a4adca4f97f0723c
紅梅陽太 <powdersugar828828@gmail.com>
2020-05-09 17:15:26 +0900

    ビルド周りの設定を追加。

--- app/next.config.js
+++ app/next.config.js
@@ -2,36 +2,37 @@ const axios = require('axios');
 require('dotenv').config();
 
 module.exports = {
+    env: {
+        access_key: process.env.API_KEY,
+    },
     exportPathMap: async function() {
         const paths = {
             '/': {page: '/'},
         };
 
-        // const headers = {
-        //     headers: {'X-API-KEY': process.env.API_KEY}
-        // };
+        const headers = {
+            headers: {'X-API-KEY': process.env.API_KEY}
+        };
 
-        // const blogRes = await axios.get(
-        //     `https://karukichi-tech-blog.microcms.io/api/v1/blogs`,
-        //     headers,
-        // );
-        // const tagRes = await axios.get(
-        //     `https://karukichi-tech-blog.microcms.io/api/v1/tags`,
-        //     headers,
-        // );
-        // const blogs = blogRes.data.contents;
-        // blogs.forEach(blog => {
-        //     paths[`/blogs/${blog.id}`] = {page: '/blogs/[id]', query: {id: blog.id}};
-        // });
+        const blogRes = await axios.get(
+            `https://karukichi-tech-blog.microcms.io/api/v1/blogs`,
+            headers,
+        );
+        const tagRes = await axios.get(
+            `https://karukichi-tech-blog.microcms.io/api/v1/tags`,
+            headers,
+        );
 
-        // const tags = tagRes.data.contents;
-        // tags.forEach(tag => {
-        //     paths[`/tags/${tag.id}`] = {page: '/tags/[id]', query: {id: tag.id}};
-        // })
+        const blogs = blogRes.data.contents;
+        for (blog of blogs) {
+            paths[`/blogs/${blog.id}`] = {page: '/blogs/[id]', query: {id: blog.id}};
+        }
+
+        const tags = tagRes.data.contents;
+        for (tag of tags) {
+            paths[`/tags/${tag.id}`] = {page: '/tags/[id]', query: {id: tag.id}};
+        }
 
         return paths;
-    },
-    env: {
-        access_key: process.env.API_KEY,
     },
 };
