d83cf7caac27bbed7254117fb1eb007cd880121c
紅梅陽太 <powdersugar828828@gmail.com>
2020-05-09 16:59:02 +0900

    エラーページを修正。

--- app/pages/_error.tsx
+++ app/pages/_error.tsx
@@ -10,11 +10,10 @@ interface Props {
 }
 
 const Error: NextComponentType<NextPageContext, {}, Props> = ({ statusCode }) => {
-    const convertString = statusCode.toFixed();
     return (
         <Layout>
-            <Head title={`${convertString}エラーが発生しました`} />
-            <H2>{convertString}: Not Found</H2>
+            <Head title={`${statusCode}エラーが発生しました`} />
+            <H2>{statusCode}: Not Found</H2>
             <p>お探しのページは一時的にアクセスができない状況にあるか、</p>
             <p>移動または削除された可能性があります。</p>
             <p>URL、ファイル名にタイプミスがないかもご確認ください。</p>
