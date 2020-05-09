a9f6d30180ad136c6d4e6b1e5c8cb256fa09ab2b
紅梅陽太 <powdersugar828828@gmail.com>
2020-05-10 01:55:46 +0900

    日付のフォーマットを変更する関数を修正。

--- app/scripts/date-format.ts
+++ app/scripts/date-format.ts
@@ -1,5 +1,5 @@
 export const dateFormat = (createdAt: string) => {
-    const reg = new RegExp('T0.+');
+    const reg = new RegExp('T.+');
     const date = createdAt.replace(reg, "");
     const formatDate = date.replace(/-/g, "\/");
     return formatDate;
