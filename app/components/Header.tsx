ab7de04d6fdb074fcd6cd321bbf04d3216a69a24
紅梅陽太 <powdersugar828828@gmail.com>
2020-05-11 00:19:17 +0900

    h1が複数にならないようにした。

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,19 +1,30 @@
-import * as React from 'react';
+import React from 'react';
+import { useRouter } from 'next/router'
 import Link from 'next/link';
 
 import { HeaderBar } from '../styled-components/organisms/AppBar';
-import { H1 } from '../styled-components/atoms/Heading';
+import { SiteTitleTop, SiteTitle } from '../styled-components/atoms/SiteTitle';
 
 interface Props {
     siteTitle: React.ReactNode;
 }
 
 const Header: React.FC<Props> = ({ siteTitle }) => {
+    const getPathName = () => {
+        const router = useRouter();
+        return router.asPath;
+    }
+
     return (
         <HeaderBar>
-            <H1 fontsize={'2rem'}><Link href="/"><a>{siteTitle}</a></Link></H1>
+            {
+                getPathName() === '/'
+                ? <SiteTitleTop><Link href="/"><a>{siteTitle}</a></Link></SiteTitleTop>
+                : <SiteTitle><Link href="/"><a>{siteTitle}</a></Link></SiteTitle>
+            }
         </HeaderBar>
     )
 }
+
 
 export default Header;
---
+++ app/styled-components/atoms/siteTitle.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+import { device } from '../MediaQuery';
+
+const baseStyle = `
+    display: inline-block;
+    font-size: 2rem;
+    font-weight: 600;
+    ${device.mobileM} {
+        font-size: 1.8rem;
+    }
+`
+
+export const SiteTitleTop = styled.h1`
+    ${baseStyle}
+`;
+
+export const SiteTitle = styled.div`
+    ${baseStyle}
+`
