fc303f5ecb6c7a02dc74b9c0eeff19a5fd08e90f
紅梅陽太 <powdersugar828828@gmail.com>
2020-05-13 21:42:11 +0900

    styled-componentsのスタイルをSSR時に生成するように変更。

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-class MyDocument extends NextDocument {
-    render() {
+class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+        const sheet = new ServerStyleSheet();
+        const originalRenderPage = ctx.renderPage;
+
+        ctx.renderPage = () =>
+            originalRenderPage({
+                enhanceApp: (App: any) => (props :any) => 
+                    sheet.collectStyles(<App {...props} />),
+        });
+
+        const initialProps = await Document.getInitialProps(ctx);
+
+        return {
+            ...initialProps,
+            styles: [...(initialProps.styles as any), ...sheet.getStyleElement()]
+        };
+    }
+
+    public render() {
         return (
             <Html lang="ja">
                 <Head>
