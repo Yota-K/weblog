fbcbabbc3f2e5a2f81155eb4b85ebba17df9d0bd
紅梅陽太 <powdersugar828828@gmail.com>
2020-05-21 22:12:13 +0900

    記事ページに以外にもデスクリプションを設定。

--- app/components/Head.tsx
+++ app/components/Head.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import Head from 'next/head';
 
 interface Props {
@@ -9,6 +10,7 @@ interface Props {
 
 export default ({ title, description, thumbnail }: Props): JSX.Element => {
     const [url, setUrl] = React.useState<string>('');
+    const defaultDescription = '駆け出しウェブエンジニアカルキチ副島が運営するウェブ系の技術をメインに書くブログです。';
 
     useEffect(() => {
         if (typeof window !== 'undefined') {
@@ -16,10 +18,23 @@ export default ({ title, description, thumbnail }: Props): JSX.Element => {
         }
     }, [url]);
 
+    const slugChecker = () => {
+        const router = useRouter();
+        const path = router.asPath;
+        const pattern = /\/blogs\/.*$/g;
+        const result = path.match(pattern);
+        return result;
+    }
+    const descriptionCheck = slugChecker();
+
     return (
         <Head>
             <title>{title}</title>
-            <meta name="Description" content={description} />
+            {
+                descriptionCheck !== null
+                ? <meta name="Description" content={description} />
+                : <meta name="Description" content={defaultDescription} />
+            }
             <link rel="shortcut icon" href="/favicon.ico" />
             <meta name="twitter:card" content="summary_large_image" />
             <meta name="twitter:site" content="@karukichi_yah" />
