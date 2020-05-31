562ee592db791060e415d796ad426146592e0060
紅梅陽太 <powdersugar828828@gmail.com>
2020-05-31 15:59:35 +0900

    関数コンポーネントの戻り値の型を指定。

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,10 +1,10 @@
-import react from 'react';
+import React from 'react';
 import { AppProps } from 'next/app';
 import 'highlight.js/styles/dracula.css';
 
 import { GlobalStyle } from '../../share/GlobalStyle';
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <>
       <GlobalStyle />
@@ -12,17 +12,5 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     </>
   );
 };
-
-// Only uncomment this method if you have blocking data requirements for
-// every single page in your application. This disables the ability to
-// perform automatic static optimization, causing every page in your app to
-// be server-side rendered.
-//
-// MyApp.getInitialProps = async (appContext) => {
-//   // calls page's `getInitialProps` and fills `appProps.pageProps`
-//   const appProps = await App.getInitialProps(appContext);
-//
-//   return { ...appProps }
-// }
 
 export default MyApp;
