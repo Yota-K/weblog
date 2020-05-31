d3bba049c2dba92d0bad4b4aa2cff052d7752587
紅梅陽太 <powdersugar828828@gmail.com>
2020-05-31 15:51:16 +0900

    Promiseの中でaxiosを呼び出していたので修正。

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,10 +1,10 @@
-const axios = require('axios');
+const axios = require('axios'); // eslint-disable-line
 import { BlogJson, Content } from '../interfaces/blog';
 import { TagJson, CategoryJson, TaxonomyJson } from '../interfaces/taxonomy';
 import { RequestHeader } from '../interfaces/request-header';
 
 export class API {
-  static BASE_URL: string = 'https://karukichi-tech-blog.microcms.io/api/v1';
+  static BASE_URL = 'https://karukichi-tech-blog.microcms.io/api/v1';
 
   private settingHeaders(): RequestHeader {
     const key = {
@@ -18,94 +18,84 @@ export class API {
   }
 
   // 投稿一覧
-  getBlog(url: string, params_id: number): Promise<BlogJson> {
-    return new Promise(async (resolve, reject) => {
-      const headers = this.getHeaders();
-      try {
-        let res;
-        if (params_id > 0) {
-          const offsetNum = 5;
-          const page = params_id * offsetNum - offsetNum;
-          res = await axios.get(`${url}/blogs?offset=${page}&limit=5`, headers);
-        } else {
-          res = await axios.get(`${url}/blogs?offset=${params_id}&limit=5`, headers);
-        }
-
-        const data = await res.data;
-        const contents = data.contents;
-        let totalCount = data.totalCount;
-        totalCount = Math.floor(totalCount / 5);
-
-        const blogObj = {
-          blogs: contents,
-          totalCount: totalCount + 1,
-        };
-
-        return resolve(blogObj);
-      } catch (er) {
-        console.log(er.status);
-        return reject(er);
+  async getBlog(url: string, params_id: number): Promise<BlogJson> {
+    const headers = this.getHeaders();
+    try {
+      let res;
+      if (params_id > 0) {
+        const offsetNum = 5;
+        const page = params_id * offsetNum - offsetNum;
+        res = await axios.get(`${url}/blogs?offset=${page}&limit=5`, headers);
+      } else {
+        res = await axios.get(`${url}/blogs?offset=${params_id}&limit=5`, headers);
       }
-    });
+
+      const data = await res.data;
+      const contents = data.contents;
+      let totalCount = data.totalCount;
+      totalCount = Math.floor(totalCount / 5);
+
+      const blogObj = {
+        blogs: contents,
+        totalCount: totalCount + 1,
+      };
+
+      return blogObj;
+    } catch (er) {
+      console.log(er.status);
+      return er;
+    }
   }
 
   // 記事ページ
-  getPost(url: string, slug: string): Promise<Content> {
-    return new Promise(async (resolve, reject) => {
-      const headers = this.getHeaders();
-      try {
-        const res = await axios.get(`${url}/blogs/${slug}`, headers);
-        const data = await res.data;
-        return resolve(data);
-      } catch (er) {
-        console.log(er.status);
-        return reject(er);
-      }
-    });
+  async getPost(url: string, slug: string): Promise<Content> {
+    const headers = this.getHeaders();
+    try {
+      const res = await axios.get(`${url}/blogs/${slug}`, headers);
+      const data = await res.data;
+      return data;
+    } catch (er) {
+      console.log(er.status);
+      return er;
+    }
   }
 
   // カテゴリーページ
-  getCategories(url: string, slug: string): Promise<CategoryJson> {
-    return new Promise(async (resolve, reject) => {
-      const headers = this.getHeaders();
-      try {
-        const res = await axios.get(`${url}/category/${slug}?depth=2`, headers);
-        const data = await res.data;
-        return resolve(data);
-      } catch (er) {
-        console.log(er.status);
-        return reject(er);
-      }
-    });
+  async getCategories(url: string, slug: string): Promise<CategoryJson> {
+    const headers = this.getHeaders();
+    try {
+      const res = await axios.get(`${url}/category/${slug}?depth=2`, headers);
+      const data = await res.data;
+      return data;
+    } catch (er) {
+      console.log(er.status);
+      return er;
+    }
   }
 
   // タグページ
-  getTags(url: string, slug: string): Promise<TagJson> {
-    return new Promise(async (resolve, reject) => {
-      const headers = this.getHeaders();
-      try {
-        const res = await axios.get(`${url}/tags/${slug}?depth=2`, headers);
-        const data = await res.data;
-        return resolve(data);
-      } catch (er) {
-        console.log(er.status);
-        return reject(er);
-      }
-    });
+  async getTags(url: string, slug: string): Promise<TagJson> {
+    const headers = this.getHeaders();
+    try {
+      const res = await axios.get(`${url}/tags/${slug}?depth=2`, headers);
+      const data = await res.data;
+      return data;
+    } catch (er) {
+      console.log(er.status);
+      return er;
+    }
   }
 
   // タグ一覧とカテゴリー一覧の取得
-  getTaxonomyList(url: string): Promise<TaxonomyJson> {
-    return new Promise(async (resolve, reject) => {
-      const headers = this.getHeaders();
-      try {
-        const res = await axios.get(`${url}/taxonomy`, headers);
-        const data = res.data;
-        return resolve(data);
-      } catch (er) {
-        console.log(er.status);
-        return reject(er);
-      }
-    });
+  async getTaxonomyList(url: string): Promise<TaxonomyJson> {
+    const headers = this.getHeaders();
+    try {
+      const res = await axios.get(`${url}/taxonomy`, headers);
+      const data = res.data;
+      return data;
+    } catch (er) {
+      console.log(er.status);
+      return er;
+    }
   }
 }
