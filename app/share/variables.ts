7fd9bfdf8970e5682c9b74d78bd48775e7b7027a
紅梅陽太 <powdersugar828828@gmail.com>
2020-06-21 17:39:06 +0900

    トップに戻るボタンを表示。

--- app/share/variables.ts
+++ app/share/variables.ts
@@ -2,6 +2,7 @@ interface KeyColor {
   baseBlack: string;
   baseBlue: string;
   subBlue: string;
+  accentBlue: string;
   borderGray: string;
   fontColor: string;
   infoColor: string;
@@ -11,6 +12,7 @@ export const colorObj: KeyColor = {
   baseBlack: '#39394d',
   baseBlue: '#331cbf',
   subBlue: '#d8e4ef',
+  accentBlue: '#7180ea',
   borderGray: '#ddd',
   fontColor: '#2b2c30',
   infoColor: '#616269',
--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { animateScroll as scroll } from 'react-scroll';
+
+import { colorObj } from '../../share/variables';
 
 interface Props {
   siteTitle: React.ReactNode;
 }
 
 const Footer: React.FC<Props> = ({ siteTitle }) => {
-  const year = new Date().getFullYear();
+  const scrollToTop = () => scroll.scrollToTop();
 
+  const handleScroll = () => {
+    const scrollBtn = document.querySelector('.scroll-top');
+    const height = window.innerHeight;
+    const offset = window.pageYOffset;
+    if (offset - height > height) {
+      scrollBtn?.classList.add('slide-top');
+    } else {
+      scrollBtn?.classList.remove('slide-top');
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', handleScroll);
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }
+  }, []);
+
+  const year = new Date().getFullYear();
   return (
     <FooterBar>
+      <svg
+        onClick={scrollToTop}
+        className="scroll-top"
+        xmlns="http://www.w3.org/2000/svg"
+        width="34"
+        height="34"
+        viewBox="0 0 24 24"
+      >
+        <path d="M0 16.67l2.829 2.83 9.175-9.339 9.167 9.339 2.829-2.83-11.996-12.17z" />
+      </svg>
       <p>
         © {year} {siteTitle}
       </p>
@@ -18,6 +52,27 @@ const Footer: React.FC<Props> = ({ siteTitle }) => {
 };
 
 const FooterBar = styled.footer`
+  .scroll-top {
+    position: fixed;
+    bottom: 18px;
+    right: 18px;
+    padding: 8px;
+    background: ${colorObj.accentBlue};
+    fill: #fff;
+    border-radius: 50%;
+    box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.12), 0 2px 2px 0 rgba(0, 0, 0, 0.24);
+    transform: translateY(1000px);
+    transition: all 0.8s;
+    cursor: pointer;
+    z-index: 10000;
+    &:hover {
+      box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.14), 0 1px 10px 0 rgba(0, 0, 0, 0.12), 0 2px 4px -1px rgba(0, 0, 0, 0.2);
+    }
+  }
+  .slide-top {
+    transform: translateY(0);
+    transition: all 0.8s;
+  }
   margin-top: 100px;
   padding: 20px;
   p {
