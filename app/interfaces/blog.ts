fb4d68724e2b32eb00c92b3fbdec5042136bd9d1
紅梅陽太 <powdersugar828828@gmail.com>
2020-08-27 20:09:43 +0900

    APIスキーマの型を変更。

--- app/interfaces/blog.ts
+++ app/interfaces/blog.ts
@@ -12,17 +12,9 @@ export interface Content {
     url: string;
   };
   category: string;
+  tags: string;
   body: string;
   description: string;
-  tag_field: {
-    id: string;
-    createdAt: string;
-    updatedAt: string;
-    name: string;
-    posts: {
-      id: string;
-    }[];
-  }[];
   category_field: {
     id: string;
     createdAt: string;
@@ -32,4 +24,13 @@ export interface Content {
       id: string;
     }[];
   };
+  tag_field: {
+    id: string;
+    createdAt: string;
+    updatedAt: string;
+    name: string;
+    posts: {
+      id: string;
+    }[];
+  }[];
 }
