a33d476c619e9f7ab70c51f265178216e5558fd7
紅梅陽太 <powdersugar828828@gmail.com>
2020-09-21 03:38:06 +0900

    サイトの情報を設定できるconfig.tsを作成。

---
+++ app/config/app.ts
@@ -0,0 +1,19 @@
+interface Config {
+  siteInfo: {
+    siteTitle: string;
+    defaultDescription: string;
+  };
+  twitterUrl: string;
+  githubUrl: string;
+  paginateNum: number;
+}
+
+export const config: Config = {
+  siteInfo: {
+    siteTitle: 'カルキチブログ',
+    defaultDescription: '駆け出しウェブエンジニアカルキチ副島が運営するウェブ系の技術をメインに書くブログです。',
+  },
+  twitterUrl: 'https://twitter.com/karukichi_yah',
+  githubUrl: 'https://github.com/Yota-K',
+  paginateNum: 5,
+};
