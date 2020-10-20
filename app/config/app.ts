1133d5cb9a5d4ab9e38246fc855ad4ed9d300f84
紅梅陽太 <powdersugar828828@gmail.com>
2020-10-20 22:29:41 +0900

    constアサーションを使用するように変更。

--- app/config/app.ts
+++ app/config/app.ts
@@ -1,6 +1,4 @@
-import { Config } from '../interfaces/config';
-
-export const config: Config = {
+export const config = {
   siteInfo: {
     siteTitle: 'カルキチのブログ',
     defaultDescription: '駆け出しウェブエンジニアカルキチ副島が運営するウェブ系の技術をメインに書くブログです。',
@@ -8,4 +6,4 @@ export const config: Config = {
   twitterUrl: 'https://twitter.com/karukichi_yah',
   githubUrl: 'https://github.com/Yota-K',
   paginateNum: 5,
-};
+} as const;
--- app/interfaces/config.ts
+++
@@ -1,9 +0,0 @@
-export interface Config {
-  siteInfo: {
-    siteTitle: string;
-    defaultDescription: string;
-  };
-  twitterUrl: string;
-  githubUrl: string;
-  paginateNum: number;
-}
