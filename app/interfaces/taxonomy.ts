1dfe3771b3b651b52037def202acc3f2aa36e1b1
紅梅陽太 <powdersugar828828@gmail.com>
2020-10-21 21:07:49 +0900

    カテゴリーとタグの型のリファクタ。

--- app/interfaces/taxonomy.ts
+++ app/interfaces/taxonomy.ts
@@ -1,26 +1,19 @@
-// カテゴリーとタグの型定義
-export interface Taxonomy {
-  id: string;
-  createdAt: string;
-  updatedAt: string;
-  name: string;
-  posts: {
-    id: string;
-  }[];
-}
-
-export interface CategoriesAndTags {
-  categories: Taxonomy[];
-  tags: Taxonomy[];
-}
-
 // カテゴリーページとタグページのビルドを行う際に使用する型定義
-// MEMO: カテゴリーとタグの型定義とほぼ一緒
-// ?をつけて必須プロパティじゃなくすることもできるけど、、、
-// ありなのか。。。
 export interface BuildTaxonomyPaginateList {
   id: string;
   posts: {
     id: string;
   }[];
+}
+
+// カテゴリーとタグの型定義
+export interface Taxonomy extends BuildTaxonomyPaginateList {
+  createdAt: string;
+  updatedAt: string;
+  name: string;
+}
+
+export interface CategoriesAndTags {
+  categories: Taxonomy[];
+  tags: Taxonomy[];
 }
