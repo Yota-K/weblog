1c63a35d68de074229e7d6ada4fdf196a58e2163
紅梅陽太 <powdersugar828828@gmail.com>
2020-11-29 01:14:54 +0900

    getServersidePropsを使わない方法を試してみた。

--- app/src/pages/search.tsx
+++ app/src/pages/search.tsx
@@ -1,13 +1,11 @@
-import { NextComponentType, NextPageContext, GetServerSideProps } from 'next';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 import { config } from '../../config/app';
 
 import { Content } from '../../interfaces/content';
-import { RecordType } from '../../interfaces/record-type';
 
 import { dateFormat } from '../../scripts/date-format';
 import { getRequestHeader } from '../../scripts/get-request-header';
@@ -26,35 +24,76 @@ import Paginate from '../components/Paginate';
 
 import Error from './_error';
 
-interface Props {
+interface Posts {
   blogs: Content[];
   totalCount: number;
 }
 
 const paginateNum = config.paginateNum;
 
-const Search: NextComponentType<NextPageContext, RecordType, Props> = ({ blogs, totalCount }) => {
+const Search: React.FC = () => {
+  const [queryString, setQueryString] = React.useState<string>('');
+  const [posts, setPosts] = React.useState<Posts>({
+    blogs: [],
+    totalCount: 0,
+  });
+
   const { siteTitle } = config.siteInfo;
 
   // 検索クエリを取得
   const router = useRouter();
   const { query } = router.query;
 
-  // 2ページ目の時 react/2
-  const paginateType = `search?query=${query}`;
+  // この部分を追加
+  useEffect(() => {
+    // idがqueryで利用可能になったら処理される
+    if (router.asPath !== router.route) {
+      // 例: React・React/2
+      setQueryString(query as string);
+    }
+  }, [router]);
 
-  // 検索クエリがない・空の配列が返ってきた時は検索失敗と判定する
-  if (!blogs.length || !query) return <Error statusCode={404} />;
+  useEffect(() => {
+    const main = async () => {
+      if (queryString) {
+        const header = getRequestHeader();
 
-  const queryString = query as string;
-  const searchResultWord = queryString.replace(/\/[0-9]+$/, '');
+        let currentPaginateNum = 1;
+        let searchWord: string;
+
+        // ２ページ目以降の場合に行う処理
+        if (queryString.match(/[0-9]+$/)) {
+          const queryParamNum = queryString.match(/[0-9]+$/) as string[];
+          currentPaginateNum = parseInt(queryParamNum[0]);
+
+          const searchQuery = queryString.replace(/\/[0-9]$/, '');
+          searchWord = encodeURI(searchQuery as string);
+        } else {
+          // matchの戻り値がnullの場合はパラメータに設定された文字列をそのままエンコードする
+          searchWord = encodeURI(queryString as string);
+        }
+
+        const offset = currentPaginateNum * paginateNum - paginateNum;
+
+        const res = await fetch(
+          `${process.env.ENDPOINT}/blogs?q=${searchWord}&offset=${offset}&limit=${paginateNum}&orders=-createdAt`,
+          header
+        );
+        const data = await res.json();
+
+        setPosts({
+          blogs: data.contents,
+          totalCount: data.totalCount,
+        });
+      }
+    };
+    main();
+  }, [queryString]);
 
   return (
     <Layout>
-      <Head title={`${searchResultWord}の検索結果｜${siteTitle}`} />
       <div id="blog-list">
-        <H2>{searchResultWord}の検索結果</H2>
-        {blogs.map((blog) => (
+        {posts.blogs.map((blog) => (
           <BlogCard key={blog.id}>
             <PostThumbnail>
               <LazyLoadImage src={blog.thumbnail.url} alt="thumbnail" effect="blur" />
@@ -88,48 +127,9 @@ const Search: NextComponentType<NextPageContext, RecordType, Props> = ({ blogs, 
             </PostInfo>
           </BlogCard>
         ))}
-        <Paginate paginateType={paginateType} totalCount={totalCount} />
       </div>
     </Layout>
   );
-};
-
-const header = getRequestHeader();
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  // 例: React・React/2
-  const { query } = context.query;
-  const queryString = query as string;
-
-  let currentPaginateNum = 1;
-  let searchWord: string;
-
-  // ２ページ目以降の場合に行う処理
-  if (queryString.match(/[0-9]+$/)) {
-    const queryParamNum = queryString.match(/[0-9]+$/) as string[];
-    currentPaginateNum = parseInt(queryParamNum[0]);
-
-    const searchQuery = queryString.replace(/\/[0-9]$/, '');
-    searchWord = encodeURI(searchQuery as string);
-  } else {
-    // matchの戻り値がnullの場合はパラメータに設定された文字列をそのままエンコードする
-    searchWord = encodeURI(queryString as string);
-  }
-
-  const offset = currentPaginateNum * paginateNum - paginateNum;
-
-  const res = await fetch(
-    `${process.env.ENDPOINT}/blogs?q=${searchWord}&offset=${offset}&limit=${paginateNum}&orders=-createdAt`,
-    header
-  );
-  const data = await res.json();
-
-  return {
-    props: {
-      blogs: data.contents,
-      totalCount: data.totalCount,
-    },
-  };
 };
 
 export default Search;
