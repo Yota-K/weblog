51e5236935d7a75b9dde1885862b89e234e64ef2
紅梅陽太 <powdersugar828828@gmail.com>
2020-11-29 14:18:17 +0900

    記事データのJSONを生成するスクリプトを追加。

---
+++ app/create-posts-json.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+require('dotenv').config();
+const fs = require('fs');
+
+(async () => {
+  const header = {
+    headers: { 'X-API-KEY': process.env.API_KEY },
+  };
+
+  const res = await axios.get(
+    `${process.env.ENDPOINT}/blogs?fields=id,title,tag_field.name&limit=9999&?orders=-publishedAt`,
+    header
+  );
+  const searchJSON = await res.data.contents;
+
+  // 書き込み処理を行う
+  fs.writeFileSync('./out/search.json', JSON.stringify(searchJSON));
+
+  console.info('検索用のJSONの生成に成功しました');
+})();
--- app/hoge.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const fs = require('fs');
-
-(async () => {
-  console.log(process.env.API_KEY);
-  console.log(process.env.ENDPOINT);
-  const searchJSON = { x: 5, y: 6 };
-  fs.writeFileSync('./out/search.json', JSON.stringify(searchJSON, null, 2));
-})();
