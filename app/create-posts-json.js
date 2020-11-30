ee82e21094902049cc62d5605f294b419d102086
紅梅陽太 <powdersugar828828@gmail.com>
2020-12-01 00:48:15 +0900

    検索用のJSON生成を行う処理のリファクタ。

--- app/create-posts-json.js
+++ app/create-posts-json.js
@@ -5,24 +5,32 @@ const fs = require('fs');
 /* eslint-disable */
 
 (async () => {
-  const header = {
-    headers: { 'X-API-KEY': process.env.API_KEY },
-  };
+  try {
+    const header = {
+      headers: { 'X-API-KEY': process.env.API_KEY },
+    };
 
-  const res = await axios.get(
-    `${process.env.ENDPOINT}/blogs?fields=id,title,tag_field.name&limit=9999&?orders=-publishedAt`,
-    header
-  );
-  const searchJSON = await res.data.contents;
+    const res = await axios.get(
+      `${process.env.ENDPOINT}/blogs?fields=id,title,tag_field.name&limit=9999&?orders=-publishedAt`,
+      header
+    );
 
-  // 書き込み処理を行う
-  if (process.env.IS_NETLIFY === 'netlify') {
-    console.info('検索用JSONの生成：Netlifyで実行します。');
-    fs.writeFileSync('./out/search.json', JSON.stringify(searchJSON));
-  } else {
-    console.info('検索用JSONの生成：開発環境で実行します。');
-    fs.writeFileSync('./public/search.json', JSON.stringify(searchJSON));
+    const searchJSON = await res.data.contents;
+
+    // 書き込み処理を行う
+    if (process.env.IS_NETLIFY) {
+      console.info('検索用JSONの生成：Netlifyで実行します。');
+      fs.writeFileSync('./out/search.json', JSON.stringify(searchJSON));
+    } else {
+      console.info('検索用JSONの生成：開発環境で実行します。');
+      fs.writeFileSync('./public/search.json', JSON.stringify(searchJSON));
+    }
+
+    console.info('検索用のJSONの生成に成功しました');
+  } catch(er) {
+    const { status, statusText } = er.respose;
+
+    console.error(`HTTP Status: ${status} ${statusText}`);
+    console.error('検索用のJSONの生成に失敗しました');
   }
-
-  console.info('検索用のJSONの生成に成功しました');
 })();
