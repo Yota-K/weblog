cf2009cc1ba00e48ed9bcd8a7263d8b90fd6bae2
紅梅陽太 <powdersugar828828@gmail.com>
2020-12-08 01:55:10 +0900

    正しいOGPを設定。

--- app/src/components/Head.tsx
+++ app/src/components/Head.tsx
@@ -27,17 +27,20 @@ export default ({ title, description, url, thumbnail }: Props): JSX.Element => {
       <meta name="twitter:site" content="@karukichi_yah" />
       <meta property="og:title" content={title} />
       <meta property="og:url" content={url} />
+      <meta property="og:locale" content="ja_JP" />
       {isBlogPage ? (
         <>
           <meta name="description" content={description} />
           <meta property="og:description" content={description} />
           <meta property="og:image" content={thumbnail} />
+          <meta property="og:type" content="article" />
         </>
       ) : (
         <>
           <meta name="description" content={defaultDescription} />
           <meta property="og:description" content={defaultDescription} />
           <meta property="og:image" content={ogpImage} />
+          <meta property="og:type" content="website" />
         </>
       )}
       {process.env.NEXT_STAGING_ENV === 'staging' && <meta name="robots" content="noindex,nofollow" />}
