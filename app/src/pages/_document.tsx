fff0f2f117f3dedb87e05d7e36ab3aec04e1fc3b
紅梅陽太 <powdersugar828828@gmail.com>
2021-01-11 19:13:11 +0900

    コードのリファクタリングとコメントアウトの追加。

--- app/src/pages/_document.tsx
+++ app/src/pages/_document.tsx
@@ -2,25 +2,46 @@ import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/do
 import React from 'react';
 import { ServerStyleSheet } from 'styled-components';
 
+// MEMO: SSR時のみ実行される
 class MyDocument extends Document {
+  // SSR実行時に生成されるページの処理をカスタマイズする
+  // css-in-js系のライブラリを使用する場合にカスタマイズが必要になる
+  //
+  // 公式: https://nextjs.org/docs/advanced-features/custom-document#customizing-renderpage
+  // https://dev.to/aprietof/nextjs--styled-components-the-really-simple-guide----101c
   static async getInitialProps(context: DocumentContext) {
     const sheet = new ServerStyleSheet();
-    const originalRenderPage = context.renderPage;
 
-    context.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App: any) => (props: any) => sheet.collectStyles(<App {...props} />),
-      });
+    try {
+      // SSRを行うページ？？
+      const page = context.renderPage;
 
-    const initialProps = await Document.getInitialProps(context);
+      // ページ内のコンポーネントからスタイルを取得する
+      context.renderPage = () =>
+        page({
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        });
 
-    return {
-      ...initialProps,
-      styles: [...(initialProps.styles as any), ...sheet.getStyleElement()],
-    };
+      // 継承元のクラスのgetInitialPropsを実行する
+      const initialProps = await Document.getInitialProps(context);
+
+      // スプレッド構文で上書き
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {/* スタイルをスタイルタグとして出力する */}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 
-  public render() {
+  render() {
     const GA_TRACKING_ID = 'G-Y2MDQ3RC4V';
 
     return (
