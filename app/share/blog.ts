9783a999f485aba30bd71c02825cdde7f656f959
紅梅陽太 <powdersugar828828@gmail.com>
2021-02-01 21:21:18 +0900

    記事本文のスタイルを別ファイルに分けた。

---
+++ app/share/blog.ts
@@ -0,0 +1,113 @@
+import styled from 'styled-components';
+
+import { colorObj } from '../share/variables';
+import { device } from '../share/media-query';
+
+export const ShareArea = styled.div`
+  button {
+    margin-right: 8px;
+  }
+`;
+
+export const MyContent = styled.div`
+  margin-top: 35px;
+  min-height: 800px;
+  word-break: break-all;
+  .eyecatch {
+    margin: -20px 0 30px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  }
+  h2 {
+    margin: 20px 0;
+    padding: 0.4rem;
+    background: #b8d0f5;
+    border-radius: 3px;
+    font-size: 1.6rem;
+    letter-spacing: 0.4px;
+    font-size: 1.4rem;
+    ${device.mobileM} {
+      font-size: 1.4rem;
+    }
+    ${device.mobileS} {
+      font-size: 1.4rem;
+    }
+  }
+  h3 {
+    position: relative;
+    margin: 20px 0;
+    padding-bottom: 5px;
+    border-bottom: 2px solid ${colorObj.borderGray};
+    &::after {
+      position: absolute;
+      bottom: -2px;
+      left: 0;
+      content: '';
+      width: 30%;
+      border-bottom: 2px solid ${colorObj.baseBlue};
+    }
+    font-size: 1.4rem;
+    ${device.mobileM} {
+      font-size: 1.2rem;
+    }
+  }
+  a {
+    color: blue;
+    text-decoration: underline;
+    font-weight: 600;
+    &:hover {
+      color: red;
+      transition: all 0.4s;
+    }
+  }
+  p {
+    font-size: 1.1rem;
+    line-height: 1.9;
+    ${device.mobileS} {
+      font-size: 1rem;
+    }
+    code {
+      border: 1px solid #ddd;
+      background-color: #eee;
+      color: #333;
+      margin: 0 2px;
+      padding: 0.1em 0.4em;
+    }
+  }
+  img {
+    width: 100%;
+    height: auto;
+    margin: 30px 0;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  }
+  ul,
+  ol {
+    margin: 25px 0;
+    padding: 0.5em 0.5em 0.5em 2em;
+    background: #fffbf4;
+    border: 2px solid #f3a6c3;
+    border-radius: 3px;
+    li {
+      font-weight: 600;
+      line-height: 1.9;
+    }
+  }
+  blockquote {
+    margin: 25px 0;
+    padding: 12px;
+    background: #ddd;
+    border-radius: 3px;
+    line-height: 1.9;
+  }
+  pre {
+    margin: 20px 0;
+    border-radius: 3px;
+    overflow-x: scroll;
+    code {
+      font-size: 0.9rem;
+      line-height: 2.2;
+    }
+  }
+  iframe {
+    margin 12px 0;
+  }
+`;
--- app/src/pages/blogs/[id].tsx
+++ app/src/pages/blogs/[id].tsx
@@ -5,7 +5,6 @@ import { NextComponentType, NextPageContext, GetStaticPaths, GetStaticProps } fr
 import Link from 'next/link';
 import React from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
-import styled from 'styled-components';
 
 import { config } from '../../../config/app';
 
@@ -15,14 +14,13 @@ import { RecordType } from '../../../interfaces/record-type';
 
 import { dateFormat } from '../../../scripts/date-format';
 
+import { ShareArea, MyContent } from '../../../share/blog';
 import { CategoryLabel } from '../../../share/CategoryLabel';
 import { getApiKey } from '../../../scripts/get-api-key';
 import { H1 } from '../../../share/Heading';
-import { device } from '../../../share/media-query';
 import { TagArea } from '../../../share/TagArea';
 import { TagLabel } from '../../../share/TagLabel';
 import { TimeStamp } from '../../../share/TimeStamp';
-import { colorObj } from '../../../share/variables';
 
 import Breadcrumb from '../../components/Breadcrumb';
 import Head from '../../components/Head';
@@ -152,114 +150,5 @@ export const getStaticProps: GetStaticProps = async (context) => {
     },
   };
 };
-
-const ShareArea = styled.div`
-  button {
-    margin-right: 8px;
-  }
-`;
-
-const MyContent = styled.div`
-  margin-top: 35px;
-  min-height: 800px;
-  word-break: break-all;
-  .eyecatch {
-    margin: -20px 0 30px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-  }
-  h2 {
-    margin: 20px 0;
-    padding: 0.4rem;
-    background: #b8d0f5;
-    border-radius: 3px;
-    font-size: 1.6rem;
-    letter-spacing: 0.4px;
-    font-size: 1.4rem;
-    ${device.mobileM} {
-      font-size: 1.4rem;
-    }
-    ${device.mobileS} {
-      font-size: 1.4rem;
-    }
-  }
-  h3 {
-    position: relative;
-    margin: 20px 0;
-    padding-bottom: 5px;
-    border-bottom: 2px solid ${colorObj.borderGray};
-    &::after {
-      position: absolute;
-      bottom: -2px;
-      left: 0;
-      content: '';
-      width: 30%;
-      border-bottom: 2px solid ${colorObj.baseBlue};
-    }
-    font-size: 1.4rem;
-    ${device.mobileM} {
-      font-size: 1.2rem;
-    }
-  }
-  a {
-    color: blue;
-    text-decoration: underline;
-    font-weight: 600;
-    &:hover {
-      color: red;
-      transition: all 0.4s;
-    }
-  }
-  p {
-    font-size: 1.1rem;
-    line-height: 1.9;
-    ${device.mobileS} {
-      font-size: 1rem;
-    }
-    code {
-      border: 1px solid #ddd;
-      background-color: #eee;
-      color: #333;
-      margin: 0 2px;
-      padding: 0.1em 0.4em;
-    }
-  }
-  img {
-    width: 100%;
-    height: auto;
-    margin: 30px 0;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-  }
-  ul,
-  ol {
-    margin: 25px 0;
-    padding: 0.5em 0.5em 0.5em 2em;
-    background: #fffbf4;
-    border: 2px solid #f3a6c3;
-    border-radius: 3px;
-    li {
-      font-weight: 600;
-      line-height: 1.9;
-    }
-  }
-  blockquote {
-    margin: 25px 0;
-    padding: 12px;
-    background: #ddd;
-    border-radius: 3px;
-    line-height: 1.9;
-  }
-  pre {
-    margin: 20px 0;
-    border-radius: 3px;
-    overflow-x: scroll;
-    code {
-      font-size: 0.9rem;
-      line-height: 2.2;
-    }
-  }
-  iframe {
-    margin 12px 0;
-  }
-`;
 
 export default Blog;
