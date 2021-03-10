6733d221ff0c8de4cddf9e3fe4d0b47714c674fa
紅梅陽太 <powdersugar828828@gmail.com>
2021-03-11 01:50:25 +0900

    APIのエンドポイントをENVで指定しないようにした。

--- app/utils/switch-contact-api.ts
+++ app/utils/switch-contact-api.ts
@@ -1,8 +1,8 @@
 export const contactApiEndpoint = () => {
   if (process.env.NEXT_STAGING_ENV === 'staging') {
-    return process.env.STG_CONTACT_ENDPOINT || '';
+    return 'https://62ywjv9wti.execute-api.ap-northeast-1.amazonaws.com/dev/send-mail';
   } else if (process.env.NEXT_PROD_ENV === 'prod') {
-    return process.env.PROD_CONTACT_ENDPOINT || '';
+    return 'https://3b1ej2urge.execute-api.ap-northeast-1.amazonaws.com/prod/send-mail';
   } else {
     return 'http://0.0.0.0:9000/dev/send-mail';
   }
