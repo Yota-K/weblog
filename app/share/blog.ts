76cb8984119ac06d747763ca85e4ecff55152f4e
紅梅陽太 <powdersugar828828@gmail.com>
2021-04-01 23:03:24 +0900

    shareディレクトリのスタイルを整理して。

--- app/share/blog.ts
+++ app/share/blog.ts
@@ -13,10 +13,12 @@ export const MyContent = styled.div`
   margin-top: 35px;
   min-height: 800px;
   word-break: break-all;
+
   .eyecatch {
     margin: -20px 0 30px;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
   }
+
   h2 {
     margin: 20px 0;
     padding: 0.4rem;
@@ -25,18 +27,15 @@ export const MyContent = styled.div`
     font-size: 1.6rem;
     letter-spacing: 0.4px;
     font-size: 1.4rem;
-    ${device.mobileM} {
-      font-size: 1.4rem;
-    }
-    ${device.mobileS} {
-      font-size: 1.4rem;
-    }
   }
+
   h3 {
     position: relative;
     margin: 20px 0;
     padding-bottom: 5px;
     border-bottom: 2px solid ${colorObj.borderGray};
+    font-size: 1.4rem;
+
     &::after {
       position: absolute;
       bottom: -2px;
@@ -45,26 +44,27 @@ export const MyContent = styled.div`
       width: 30%;
       border-bottom: 2px solid ${colorObj.baseBlue};
     }
-    font-size: 1.4rem;
+
     ${device.mobileM} {
       font-size: 1.2rem;
     }
   }
+
   a {
     color: blue;
     text-decoration: underline;
     font-weight: 600;
+
     &:hover {
       color: red;
       transition: all 0.4s;
     }
   }
+
   p {
     font-size: 1.1rem;
     line-height: 1.9;
-    ${device.mobileS} {
-      font-size: 1rem;
-    }
+
     code {
       border: 1px solid #ddd;
       background-color: #eee;
@@ -72,13 +72,19 @@ export const MyContent = styled.div`
       margin: 0 2px;
       padding: 0.1em 0.4em;
     }
+
+    ${device.mobileS} {
+      font-size: 1rem;
+    }
   }
+
   img {
     width: 100%;
     height: auto;
     margin: 30px 0;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
   }
+
   ul,
   ol {
     margin: 25px 0;
@@ -86,11 +92,13 @@ export const MyContent = styled.div`
     background: #fffbf4;
     border: 2px solid #f3a6c3;
     border-radius: 3px;
+
     li {
       font-weight: 600;
       line-height: 1.9;
     }
   }
+
   blockquote {
     margin: 25px 0;
     padding: 12px;
@@ -98,15 +106,18 @@ export const MyContent = styled.div`
     border-radius: 3px;
     line-height: 1.9;
   }
+
   pre {
     margin: 20px 0;
     border-radius: 3px;
     overflow-x: scroll;
+
     code {
       font-size: 0.9rem;
       line-height: 2.2;
     }
   }
+
   iframe {
     margin 12px 0;
   }
