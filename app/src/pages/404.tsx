e83bc917ee94f5e29e5c5324b1a40ef476ddc0fd
紅梅陽太 <powdersugar828828@gmail.com>
2021-06-13 17:47:00 +0900

    404.tsxを追加して。

---
+++ app/src/pages/404.tsx
@@ -0,0 +1,32 @@
+import { NextComponentType, NextPageContext } from 'next';
+import React from 'react';
+
+import { RecordType } from '../../interfaces/record-type';
+
+import { H2 } from '../../share/Heading';
+
+import Head from '../components/Head';
+import Layout from '../components/Layout';
+
+interface Props {
+  statusCode: number;
+}
+
+const Error: NextComponentType<NextPageContext, RecordType, Props> = ({ statusCode }) => {
+  return (
+    <Layout>
+      <Head title={`${statusCode}エラーが発生しました`} />
+      <H2>{statusCode} Not Found</H2>
+      <p>お探しのページは一時的にアクセスができない状況にあるか、</p>
+      <p>移動または削除された可能性があります。</p>
+      <p>URL、ファイル名にタイプミスがないかもご確認ください。</p>
+    </Layout>
+  );
+};
+
+Error.getInitialProps = async ({ res, err }: NextPageContext) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 400;
+  return { statusCode };
+};
+
+export default Error;
