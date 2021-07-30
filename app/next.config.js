05a36f3ee84881e37e7178757a0e8b44ae2f1de3
Y.K <powdersugar828828@gmail.com>
2021-07-30 23:17:42 +0900

    feat: Next.js@11.0.0からは、ESLintがデフォルトで搭載されたのでそれの対応。

--- app/next.config.js
+++ app/next.config.js
@@ -5,6 +5,9 @@ module.exports = {
     ENDPOINT: process.env.ENDPOINT,
     RECAPTCHA_KEY: process.env.RECAPTCHA_KEY,
   },
+  eslint: {
+    dirs: ['share/', 'src/components/', 'src/pages/', 'src/lib/', 'utils/'],
+  },
   webpack: (config, { dev }) => {
     // Docker環境で動かしているため、ポーリングの設定を行う
     if (dev) {
