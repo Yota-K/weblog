11d4060e9f9d01e1fc1fb0728bfab766ee562a48
Y.K <powdersugar828828@gmail.com>
2021-08-07 00:31:10 +0900

    fix: 手違いでステージング用のメール送信のエンドポイントが変わってしまったので修正。

--- app/utils/switch-contact-api.ts
+++ app/utils/switch-contact-api.ts
@@ -5,7 +5,7 @@ export const contactApiEndpoint = () => {
   if (host === 'karukichi-blog.netlify.app') {
     return 'https://3b1ej2urge.execute-api.ap-northeast-1.amazonaws.com/prod/send-mail';
   } else if (host === 'staging--karukichi-blog.netlify.app') {
-    return 'https://62ywjv9wti.execute-api.ap-northeast-1.amazonaws.com/dev/send-mail';
+    return 'https://mlrwxvphj2.execute-api.ap-northeast-1.amazonaws.com/dev/send-mail';
   } else {
     return 'http://0.0.0.0:9000/dev/send-mail';
   }
