454e161fb0289c1added06fc0f221308780d2711
Y.K <powdersugar828828@gmail.com>
2021-12-07 00:04:00 +0900

    fix: SearchJsonの型定義が間違っていたので修正、

--- app/src/types/search-posts.ts
+++ app/src/types/search-posts.ts
@@ -3,4 +3,6 @@ import { TaxonomyField, Content } from '@/types/content';
 type PostIdAndTitle = Pick<Content, 'id' | 'title'>;
 type TagName = Pick<TaxonomyField, 'name'>;
 
-export type SearchJson = PostIdAndTitle & TagName;
+export type SearchJson = PostIdAndTitle & {
+  tag_field: TagName[];
+};
