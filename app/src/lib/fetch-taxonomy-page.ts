cb59b6fb693086d4e0bc32595db4184afa7764cd
Y.K <powdersugar828828@gmail.com>
2021-12-11 23:18:18 +0900

    feat: カテゴリーとタグページを生成するために必要なデータを取ってくる関数名の変更。

--- app/src/lib/fetch-taxonomy-page.ts
+++ app/src/lib/fetch-taxonomy-page.ts
@@ -5,9 +5,9 @@ type TaxonomyType = 'category' | 'tags';
 /**
  * カテゴリーページ・タグページを表示するために必要な情報を取得
  * @param endpoint APIのエンドポイント
- * @param fields *コンテンツの中で取得する要素を指定
+ * @param fields コンテンツの中で取得する要素を指定
  */
-export const fetchTaxonomiesData = async <T>(endpoint: TaxonomyType, fields: string) => {
+export const fetchTaxonomyPage = async <T>(endpoint: TaxonomyType, fields: string) => {
   const data = await client.get<T>({
     endpoint,
     queries: {
