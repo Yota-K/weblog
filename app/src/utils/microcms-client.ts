9fd4c19cfe61d5c39111433975cb4db720eece16
Y.K <powdersugar828828@gmail.com>
2023-03-26 22:03:55 +0900

    delete: 多分いらなくなったから削除。

--- app/src/utils/microcms-client.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from 'microcms-js-sdk';
-import { config } from '@/config/app';
-
-const { serviceDomain } = config.siteInfo;
-
-export const client = createClient({
-  serviceDomain,
-  apiKey: process.env.API_KEY || '',
-});
