4f1e3e4347b60a9dd89ff5d9016820c767c444dc
Y.K <powdersugar828828@gmail.com>
2023-04-08 00:52:22 +0900

    feat: Set up Linaria.

--- app/next.config.js
+++ app/next.config.js
@@ -1,10 +1,7 @@
-module.exports = {
+const withLinaria = require('next-linaria');
+
+module.exports = withLinaria({
   // target: 'serverless',
-  env: {
-    API_KEY: process.env.API_KEY,
-    ENDPOINT: process.env.ENDPOINT,
-    RECAPTCHA_KEY: process.env.RECAPTCHA_KEY,
-  },
   eslint: {
     // ビルド時にチェックを実行しない
     // falseの状態だと、エラーではなく警告でもビルドが通らなくなる
@@ -12,7 +9,7 @@ module.exports = {
     ignoreDuringBuilds: true,
     dirs: ['share/', 'src/components/', 'src/pages/', 'src/lib/', 'utils/'],
   },
-  webpack: (config, { dev }) => {
+  webpack(config, { dev }) {
     // Docker環境で動かしているため、ポーリングの設定を行う
     if (dev) {
       config.watchOptions = {
@@ -20,10 +17,9 @@ module.exports = {
         aggregateTimeout: 200,
       };
     }
-
     return config;
   },
   swcMinify: false,
   // page.tsxという拡張子以外ページとして認識しないようにする
   pageExtensions: ['page.tsx'],
-};
+});
